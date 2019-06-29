2028d00a1e79bd7ec94793a17f6cd3a682757359
RodrigoDisselli <35853479+RodrigoDisselli@users.noreply.github.com>
2019-06-29 10:57:59 -0300

    Setup local files

---
+++ js/go_to.js
@@ -0,0 +1,8 @@
+function goTo(div) {
+    document.getElementById(div).scrollIntoView({behavior: "smooth"})
+}
+
+function openWhats(){
+    window.open('https://wa.me/5519971670076?text=Olá%20eu%20gostaria%20de%20agendar%20uma%20consulta');
+}
+
